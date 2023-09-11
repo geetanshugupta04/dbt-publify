449e39697d9a3fd5f2af9cb18da8cb5f8d5061a2
geetanshugupta04@gmail.com <geetanshugupta04@gmail.com>
2023-09-11 08:55:44 +0000

    temporarily deleting old code

--- models/marts/core/intermediate/int_impressions.sql
+++
@@ -1,7 +0,0 @@
-with 
-impressions as (
-
-    select * from {{ ref('stg_python')}}
-)
-
-select * from impressions
--- models/staging_teleads/stg_bid_request_user.sql
+++
@@ -1,9 +0,0 @@
-select 
-
-_airbyte_data:_id,
-_airbyte_data:createdOn,
-
-_airbyte_data:user.buyeruid
-
-from bidrequesttele.bid_request_schema._airbyte_raw_bidrequestad 
-limit 100;
